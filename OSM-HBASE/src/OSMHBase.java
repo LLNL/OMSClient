ade37472bfe2c71949a4dd7549604439562a18c1
Kevin A. Brown <kevin.brown.jm@gmail.com>
2015-08-17 18:21:13 -0700

    WIP: wrote counters from all snapshots in a single history file in JSON format.

--- OSM-HBASE/src/OSMHBase.java
+++ OSM-HBASE/src/OSMHBase.java
@@ -18,30 +18,66 @@ import gov.llnl.lc.infiniband.core.IB_Guid;
 import gov.llnl.lc.infiniband.core.IB_Link;
 import gov.llnl.lc.infiniband.opensm.plugin.data.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class OSMHBase {
+	
+	private static BufferedWriter countersBW, linksBW, routesBW;
+	
 	public static void main(String[] args) throws Exception
 	{
 
 		OMS_Collection omsHistory = null;
-		try
-		{	int i;
-			long timestamp;
-			omsHistory = OMS_Collection.readOMS_Collection("/Users/brown303/workspace/llnl/networks/oms/cab.20150730.his");
-			OpenSmMonitorService oms = omsHistory.getNewestOMS();
-			OSM_Fabric fabric = oms.getFabric();
+		OpenSmMonitorService oms = null;
+		OSM_Fabric fabric = null;
+		
+		long currentTime = System.currentTimeMillis() / 1000;
+		long timestamp;
+		
+		int i;
+		
+		try{	
+			File counterFile = new File("/Users/brown303/workspace/eclipse/OSM-HBASE/data/counters." + currentTime +".txt");
+			File routesFile = new File("/Users/brown303/workspace/eclipse/OSM-HBASE/data/routes." + currentTime + ".txt");
+			File linksFile = new File("/Users/brown303/workspace/eclipse/OSM-HBASE/data/links." + currentTime + ".txt");
+			if (!counterFile.exists()) { counterFile.createNewFile(); }
+			if (!routesFile.exists()) { routesFile.createNewFile(); }
+			if (!linksFile.exists()) { linksFile.createNewFile(); }
 			
-			timestamp = oms.getTimeStamp().getTimeInSeconds();
+			countersBW = new BufferedWriter(new FileWriter(counterFile));
+			routesBW = new BufferedWriter(new FileWriter(routesFile));
+			linksBW = new BufferedWriter(new FileWriter(linksFile));
+
+			
+			omsHistory = OMS_Collection.readOMS_Collection("/Users/brown303/workspace/llnl/networks/oms/cab.20150730.his");
+			//oms = omsHistory.getNewestOMS();
+			//fabric = oms.getFabric();
+			
 			
 			//writePortForwardingTable(RT_Table.buildRT_Table(fabric), timestamp);
 			//writeLinks(fabric.getIB_Links(), timestamp);
 			
-			writePortCounters(fabric.getOSM_Ports(), timestamp);
 			
-			//for (i = 50; i < omsHistory.getSize(); i++){
-				//oms = omsHistory.getOMS(i);
-				//System.out.println(i + " - " + oms.getTimeStamp().toString() + " - " + oms.getTimeStamp().getTimeInSeconds());
-			//}
 			
+			for (i = 0; i < omsHistory.getSize(); i++){
+				oms = omsHistory.getOMS(i);
+				fabric = oms.getFabric();
+				
+				timestamp = oms.getTimeStamp().getTimeInSeconds();
+				System.out.println("Processing history file: " + oms.getTimeStamp().toString() + ".");
+				
+				writePortCounters(fabric.getOSM_Ports(), timestamp);
+			}
+			
+			countersBW.flush();
+			countersBW.close();
+			
+			routesBW.flush();
+			routesBW.close();
+			
+			linksBW.flush();
+			linksBW.close();
 		}
 		catch (Exception e)
 		{
@@ -51,6 +87,29 @@ public class OSMHBase {
 		System.out.println("- Complete");
 	}
 	
+	private static void writeFileHeaders(){
+		try{
+			countersBW.write("# Record format: \"<timestamp>:<nguid>:<pn>:<recvData>:<xmitData>\""); 
+			countersBW.newLine();
+			countersBW.write("# ----Port Counters BEGIN----");
+			
+			routesBW.write("# Record format: \"<ExitPort>:<LID>\"");
+			routesBW.newLine();
+			routesBW.write("#----Forwarding Table BEGIN----");
+			
+			linksBW.write("# Record format: \"nguid1:nguid2:pn1:pn2:ntype1:ntype2:lid1:lid2\"");
+			linksBW.newLine();
+			linksBW.write("#----Links BEGIN----");
+			
+		}catch(Exception e){
+			System.out.println("ERROR: write file headers.");
+			
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+	}
+	
 	private static void writePortCounters(LinkedHashMap<String, OSM_Port> ports, long timestamp){
 		int i;
 		
@@ -58,33 +117,62 @@ public class OSMHBase {
 		String portId;
 		String nguid;
 		int portNum;
-		long recvData;
-		long xmitData;
-		StringBuffer buffer = new StringBuffer();
-		
-		buffer.append("# Record format: \"<timestamp>:<nguid>:<pn>:<recvData>:<xmitData>\"\n");
-		buffer.append("----Port Counters BEGIN----\n");
+		long recvData, xmitDrop, xmitWait, recvErr, recvSwRE, recvRPhE, xmitData, xmitConE;
+		JSONObject jsonPort = null;
 		
 		i = 0;
 		for (Map.Entry<String, OSM_Port> entry: ports.entrySet()){
+			
 			portId = entry.getKey();
 			port = entry.getValue();
 			
 			nguid = portId.substring(0, 19).replace(":", "");
 			portNum = Integer.parseInt(portId.substring(20));
 			
+			if (port.getPfmPort() == null){
+				continue;
+			}
+			timestamp = port.pfmPort.counter_ts;
 			recvData = port.pfmPort.getCounter(PFM_Port.PortCounterName.rcv_data);
+			recvErr = port.pfmPort.getCounter(PFM_Port.PortCounterName.rcv_err);
+			recvSwRE = port.pfmPort.getCounter(PFM_Port.PortCounterName.rcv_switch_relay_err);
+			recvRPhE = port.pfmPort.getCounter(PFM_Port.PortCounterName.rcv_rem_phys_err);
 			xmitData = port.pfmPort.getCounter(PFM_Port.PortCounterName.xmit_data);
+			xmitDrop = port.pfmPort.getCounter(PFM_Port.PortCounterName.xmit_discards);
+			xmitWait = port.pfmPort.getCounter(PFM_Port.PortCounterName.xmit_wait);
+			xmitConE = port.pfmPort.getCounter(PFM_Port.PortCounterName.xmit_constraint_err);
 			
-			//buffer.append(timestamp + ":" + nguid + ":" + portNum + ":" + recvData + ":" + xmitData + "\n"); 
 			
-			if (i == 10){
-				//System.out.println(buffer.toString());
+			jsonPort = new JSONObject();
+			try {
+				jsonPort.put("ts", timestamp);
+				jsonPort.put("nguid", nguid);
+				jsonPort.put("portNum", portNum);
+				
+				jsonPort.put("recv_data", recvData);
+				jsonPort.put("rcv_err", recvErr);
+				jsonPort.put("rcv_switch_relay_err", recvSwRE);
+				jsonPort.put("rcv_rem_phys_err", recvRPhE);
+				
+				jsonPort.put("xmit_data", xmitData);
+				jsonPort.put("xmit_discards", xmitDrop);
+				jsonPort.put("xmit_wait", xmitWait);
+				jsonPort.put("xmit_constraint_err", xmitConE);
+				
+				countersBW.write(jsonPort.toString());
+				countersBW.newLine();
+				
+				//if (i == 2) break;
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("ERROR: could not write port counters. ");
+				
+				e.printStackTrace();
+				System.exit(1);
 			}
 			i++;
 		}
-				
-		//System.out.println("Done: " + i);
 	}
 	
 	private static void writePortForwardingTable(RT_Table RoutingTable, long timestamp){
@@ -94,41 +182,25 @@ public class OSMHBase {
 		RT_Port port;
 		int portNum;
 		int routeLid;
-		StringBuffer buffer = new StringBuffer();
-	
-		buffer.append("# Record format: \"<ExitPort>:<LID>\"\n");
-		buffer.append("----Forwarding Table BEGIN----\n");
-		
-		for (Map.Entry<String, RT_Node> nEntry: RoutingTable.getSwitchGuidMap().entrySet()){
-			node  = nEntry.getValue();
-			nguid = node.getGuid().toColonString().replace(":", "");
-			
-			buffer.append("\nSwitch: 0x" + nguid + "\n");
-			
-			for (Map.Entry<String,RT_Port> pEntry: node.getPortRouteMap().entrySet()){
-				port = pEntry.getValue();
-				portNum = port.getPortNumber();
-				
-				for (Map.Entry<String,Integer> item: port.getLidGuidMap().entrySet()){
-					routeLid = item.getValue();
-					buffer.append(portNum + ":" + routeLid + "\n");
-				}
-			}
-		}
-		
-		buffer.append("\n----END----");
 		
 		try{
-			File file = new File("/Users/brown303/workspace/eclipse/OSM-HBASE/data/routes-table." + timestamp + ".txt");
-			if (!file.exists()) {
-				file.createNewFile();
+			for (Map.Entry<String, RT_Node> nEntry: RoutingTable.getSwitchGuidMap().entrySet()){
+				node  = nEntry.getValue();
+				nguid = node.getGuid().toColonString().replace(":", "");
+				
+				routesBW.newLine();
+				routesBW.write("Switch: 0x" + nguid); routesBW.newLine();
+				
+				for (Map.Entry<String,RT_Port> pEntry: node.getPortRouteMap().entrySet()){
+					port = pEntry.getValue();
+					portNum = port.getPortNumber();
+					
+					for (Map.Entry<String,Integer> item: port.getLidGuidMap().entrySet()){
+						routeLid = item.getValue();
+						routesBW.write(portNum + ":" + routeLid); routesBW.newLine();
+					}
+				}
 			}
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(buffer.toString());
-			bw.close();
-			System.out.println("- Wrote routes file.");
-			
 		}catch (Exception e){
 			System.out.println("ERROR: Unable to write routes to file.");
 		}
@@ -140,10 +212,6 @@ public class OSMHBase {
 		Integer portNum1, portNum2;
 		String nodeType1, nodeType2;
 		Integer lid1, lid2;
-		
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("# Record format: \"nguid1:nguid2:pn1:pn2:ntype1:ntype2:lid1:lid2\"\n");
-		buffer.append("----BEGIN----\n\n");
 		
 		for(Map.Entry<String, IB_Link> entry: ibLinks.entrySet()){
 	        IB_Link ln = entry.getValue();
@@ -163,25 +231,15 @@ public class OSMHBase {
 	        lid1 = port1.getAddress().getLocalId();
 	        lid2 = port2.getAddress().getLocalId();
 	        
-	        buffer.append(nguid1 + ":" + nguid2 + ":" + portNum1  + ":" + portNum2  + ":" + nodeType1 + ":" + nodeType2 + ":" + lid1 + ":" + lid2 + "\n");
-		}
-
-		buffer.append("\n----END----");
-		
-		try{
-			File file = new File("/Users/brown303/workspace/eclipse/OSM-HBASE/data/links-table." + timestamp + ".txt");
-			if (!file.exists()) {
-				file.createNewFile();
+	        try{
+		        linksBW.write(nguid1 + ":" + nguid2 + ":" + portNum1  + ":" + portNum2  + ":" + nodeType1 + ":" + nodeType2 + ":" + lid1 + ":" + lid2);
+		        linksBW.newLine();
+	        }catch (Exception e){
+				System.out.println("ERROR: Unable to write links to file.");
 			}
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(buffer.toString());
-			bw.close();
-			System.out.println("- Wrote links file.");
-			
-		}catch (Exception e){
-			System.out.println("ERROR: Unable to write links to file.");
 		}
+		System.out.println("- Wrote links file.");
+
 	}
 	
 	public static void WorkConsole(String[] args) throws Exception
